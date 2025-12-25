1c766cd273a8b00c55e5af373cf4dd8313f6ae98
Sneha Patil <ssp142005@gmail.com>
2025-12-25 08:43:03 +0530

    ip  changed

--- Database/pool.js
+++ Database/pool.js
@@ -3,7 +3,7 @@ const pool=mysql2.createPool({
     host:"10.49.246.108",
     user:"project",
     password:"manager",
-    database:"MERN_Project"
+    database:"project_db"
 })
 
 module.exports=pool
